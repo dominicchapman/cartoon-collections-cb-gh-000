352888aaa1b7a9ec47179de8a8a97c485a3e890d
Dominic Chapman <dominicchapman@me.com>
2017-10-04 23:50:27 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,7 +10,13 @@ def long_planeteer_calls(array)
   array.any? { |item| item.length > 4 }
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+
+  # find returns first for which block is not false
+  array.find do |item|
+    # does the array cheese_types include "tomato soup"? false. so, next.
+    # does the array cheese_types include "cheddar"? true. so find will return it.
+    cheese_types.include?(item)
+  end
 end
